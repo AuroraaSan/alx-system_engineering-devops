24df88ff3372bd24f392a9678a524a434395c050
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-08 22:49:00 +0200

    fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
 
 
-def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
-
-    headers = {"User-Agent": "Reddit API Client by /u/your_username"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print('None')
-        return
-
-    data = response.json().get("data")
-    if not data or "children" not in data:
-        print('None')
-        return
-
-    for child in data["children"]:
-        title = child["data"].get("title")
-        if title:
-            print(title)
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        top_ten(sys.argv[1])
+        return 0

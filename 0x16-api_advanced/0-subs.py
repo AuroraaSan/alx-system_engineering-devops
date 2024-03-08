1eb3e9979a1fbee881e8b2aee4d158ea5f19fda4
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-08 22:39:53 +0200

    fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,22 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    headers = {"User-Agent": "My API Client"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    data = response.json().get("data")
+    if not data:
+        return 0
+
+    subscribers = data.get("subscribers")
+    if subscribers is None:
+        return 0
+
+    return subscribers
 
 if __name__ == '__main__':
     import sys

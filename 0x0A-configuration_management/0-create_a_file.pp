2acae7c4d0b71ef17fac1f499a35850788dc8979
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2024-03-24 23:41:58 +0200

    nn

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create a file in /tmp.
 
-file { 'holberton':
-  ensure  => 'present',
-  path    => '/tmp/holberton',
-  content => 'I love Puppet',
-  group   => 'www-data',
+file { '/tmp/school':
+  ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
